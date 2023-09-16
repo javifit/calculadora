d3c23a113d24fd7630e7b793277a16344290e239
javifit <bodybuilding.jr@gmail.com>
2023-09-16 00:49:31 -0300

    Agregado cambio de tema en la calculadora

--- ProyectoCalculadora.py
+++ ProyectoCalculadora.py
@@ -71,4 +71,56 @@ tk.Button(ventana, text='C', padx=20, pady=20, font=('Arial', 20),
 tk.Button(ventana, text='=', padx=20, pady=20, font=('Arial', 20),
           command=calcular).grid(row=row_val, column=col_val+1)
 
-ventana.mainloop()
+
+
+# TEMA CLARO O OSCURO PARA LA CALCULADORA
+# Variables globales para el tema
+ 
+TEMA_CLARO = 'light'
+TEMA_OSCURO = 'dark'
+TEMA_ACTUAL = TEMA_CLARO
+
+def cambiar_tema():
+    global TEMA_ACTUAL
+    TEMA_ACTUAL = TEMA_OSCURO if TEMA_ACTUAL == TEMA_CLARO else TEMA_CLARO
+
+    ventana.tk_setPalette(background=COLORES[TEMA_ACTUAL]['background'], foreground=COLORES[TEMA_ACTUAL]['foreground'])
+
+    for boton in botones:
+        boton.configure(background=COLORES[TEMA_ACTUAL]['button_background'], foreground=COLORES[TEMA_ACTUAL]['button_foreground'])
+
+# Definir colores para los temas
+COLORES = {
+    'light': {
+        'background': '#FFFFFF',
+        'foreground': '#000000',
+        'button_background': '#E0E0E0',
+        'button_foreground': '#000000'
+    },
+    'dark': {
+        'background': '#333333',
+        'foreground': '#FFFFFF',
+        'button_background': '#444444',
+        'button_foreground': '#FFFFFF'
+    }
+}
+
+ventana = tk.Tk()
+ventana.title("Calculadora")
+
+# Configurar el tema predeterminado (claro)
+ventana.tk_setPalette(background=COLORES[TEMA_CLARO]['background'], foreground=COLORES[TEMA_CLARO]['foreground'])
+
+# Botones (solo como ejemplo)
+botones = []
+for i in range(3):
+    for j in range(3):
+        boton = tk.Button(ventana, text=f'Botón {i+1}-{j+1}', padx=20, pady=20)
+        boton.grid(row=i, column=j)
+        botones.append(boton)
+
+# Crear un botón para cambiar el tema
+tk.Button(ventana, text='Cambiar Tema', command=cambiar_tema).grid(row=3, column=0, columnspan=3)
+
+
+ventana.mainloop()                                                                                                         
